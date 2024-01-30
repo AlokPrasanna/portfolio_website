7d3ed5096e948acf2a99406e9d19abc3d70e0255
AlokPrasanna <alokprasanna1104@gmail.com>
2024-01-31 00:35:41 +0530

    Added Navigation Links Component

--- src/app/Components/NavigationBar.jsx
+++ src/app/Components/NavigationBar.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import NavigationLinks from './NavigationLinks';
+import {Bars3Icon , XMarkIcon} from "@heroicons/react/24/solid";
+
+const NavLinks = [
+    {
+        title:"About",
+        path:"#about"
+    },
+    {
+        title:"Projects",
+        path:"#projects"
+    },
+    {
+        title:"Contact",
+        path:"#contact"
+    }
+]
 
 const NavigationBar = () => {
+    const [NavBarOpen,setNavBarOpen] = useState(false);
+    
   return (
-    <div>
-      
-    </div>
+    <nav className='fixed top-0 right-0 left-0 z-10 bg-[#121212] bg-opacity-90 '>
+      <div className='flex flex-wrap items-center justify-between mx-auto p-4'>
+        <Link href={"/"} className=' text-2xl md:text-4xl text-white font-semibold'>LOGO</Link>
+        <div className='mobile-menu block md:hidden'>
+            {
+                !NavBarOpen ? (
+                    <button onClick={() => setNavBarOpen(true)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                        <Bars3Icon className='h-5 w-5' />
+                    </button>
+                ) : (
+                    <button onClick={() => setNavBarOpen(false)} className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                        <XMarkIcon className='h-5 w-5' />
+                    </button>
+                )
+            }
+        </div>
+        <div className='menu hidden md:block md:w-auto' id="navbar">
+            <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                {NavLinks.map((link,index) => (
+                    <li key={index}><NavigationLinks href={link.path} title={link.title}/></li>
+                ))}
+            </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "./Components/HeroSection";
+import NavigationBar from "./Components/NavigationBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <NavigationBar />
+      <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection/>
       </div>
     </main>
