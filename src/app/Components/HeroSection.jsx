654bbb041e802bd4cc8eaffbc794951a2c034d04
AlokPrasanna <alokprasanna1104@gmail.com>
2024-01-30 12:00:58 +0530

    Added Component (HeroSection)

---
+++ src/app/Components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <section>
+        <h1 className='text-white'>Hello, I'm Alok</h1>
+      </section>
+    </div>
+  )
+}
+
+export default HeroSection
+
