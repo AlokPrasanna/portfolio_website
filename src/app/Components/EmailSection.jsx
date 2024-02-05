50507efda224cb4f0a51b9122d8a576e40b7cabd
AlokPrasanna <alokprasanna1104@gmail.com>
2024-02-05 12:40:36 +0530

    Added Github and LinkedIn urls

--- src/app/Components/EmailSection.jsx
+++ src/app/Components/EmailSection.jsx
@@ -44,8 +44,8 @@ const EmailSection = () => {
             I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you! Let's connect and explore exciting possibilities together.
         </p>
         <div className='socials flex flex-row gap-2'>
-          <Link href="github.com"><Image src={GithubIcon} alt='Github Icon' /></Link>
-          <Link href="linkedin.com"><Image src={LinkedinIcon} alt='Linkedin Icon' /></Link>
+          <Link href="https://github.com/AlokPrasanna"><Image src={GithubIcon} alt='Github Icon' /></Link>
+          <Link href="https://www.linkedin.com/in/alokprasanna/"><Image src={LinkedinIcon} alt='Linkedin Icon' /></Link>
         </div>
       </div>
       <div>
