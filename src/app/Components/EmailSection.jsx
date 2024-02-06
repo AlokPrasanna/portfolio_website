86c6e6c0a5ec1450c1f5bea209e90e83d3c19779
AlokPrasanna <alokprasanna1104@gmail.com>
2024-02-06 11:18:12 +0530

    Modified HeroSection and EmailSection

--- src/app/Components/EmailSection.jsx
+++ src/app/Components/EmailSection.jsx
@@ -60,7 +60,7 @@ const EmailSection = () => {
           </div> 
           <div className='mb-6'>
             <label htmlFor='message' className='text-white block text-sm mb-2 font-medium'>Message</label>
-            <textarea name='message' id='message' className='bg-[#18191E] border-[#3335F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder="Let's talk about..." />
+            <textarea name='message' id='message' className='bg-[#18191E] border-[#3335F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder={"Let's talk about..."} />
           </div> 
           <button type='submit' className='bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message</button>
           {
--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
             <motion.div initial={{opacity:0, scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}} className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
                 <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold'>
                     <span className='text-transparent bg-clip-text bg-gradient-to-br from-primary-400 to-secondary-600'>
-                        Hello,  I&apos;m{" "}
+                        Hello,  I'm{" "}
                     </span><br />
                     <TypeAnimation
                         sequence={[
